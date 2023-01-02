771e674efa48d196804f46e792272fd422ac1cdf
vchauha4 <60143557+vchauha4@users.noreply.github.com>
2023-01-02 12:13:58 -0500

    Added dropdown autocomplete for search bar, and implemented MUI card

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import SearchBar from './SearchBar';
 function Header() {
   return (
     <div style={{ backgroundColor: 'purple', color: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <h1 style={{ marginLeft: '10px' }}>findmyGC</h1>
-      <input type="text" placeholder="Search for a group chat" />
+      <SearchBar/>
       <button style={{ marginRight: '10px' }}>Add new group chat</button >
     </div>
   );
