39e8f75cef0275ff4418d3ea59132dc34cc7a474
vchauha4 <vchauha4@uwo.ca>
2023-01-03 16:42:04 -0500

    small changes

--- my-app/src/SearchBar.js
+++ my-app/src/SearchBar.js
@@ -3,6 +3,7 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
 
+
  function SearchBar() {
   return (
     <Stack spacing={2} sx={{ width: 300 }}>
@@ -31,6 +32,7 @@ import Autocomplete from '@mui/material/Autocomplete';
 export default SearchBar;
 
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+
 const top100Films = [
   { title: 'The Shawshank Redemption', year: 1994 },
   { title: 'The Godfather', year: 1972 },
