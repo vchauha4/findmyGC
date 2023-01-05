3b839f7bae20095ece2cf1c9d4e9ad7c0d06ddb3
vchauha4 <vchauha4@uwo.ca>
2023-01-05 13:57:14 -0500

    Added more backend stuff

--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -1,18 +1,37 @@
-const { appBarClasses } = require("@mui/material")
-const express = require("express")
+const { appBarClasses, CssBaseline } = require("@mui/material")
 
+const express = require("express")
+const {connectToDb, getDB} = require('./db')
 
 //init app
 
 const app = express()
 
-app.listen(3001, ()=>{
+//db connection
+let db
 
-    console.log('app listening on port 3001')
-}
-)
+connectToDb((err) => {
+    if(!err){
+        app.listen(3001, ()=>{
+            console.log('app listening on port 3001')
+        })
+        db = getDB()
+    }
+})
 
-app.get('/books', (req,res)=> {
+
+app.get('/departments', (req,res)=> {
+    let departments = []
+    db.collection('department')
+        .find() //cursor toArray forEach
+        .forEach(department => departments.push(department))
+        .then(()=>{
+            res.status(200).json(departments)
+        })
+        .catch(()=>{
+            res.status(500).json({error:'could not fetch documents'})
+        })
+
     res.json({mssg : "welcome to the api"})
 
 })
